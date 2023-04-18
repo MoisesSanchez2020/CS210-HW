768390d52a28085662c4220b0a9feecafab7571b
MoisesSanchez2020 <san19039@byui.edu>
2023-04-18 14:36:34 -0700

    first commit prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write(" What is your grade percentage");
+       string  percentage = Console.ReadLine();
+       int numGrade = int.Parse(percentage);
+
+         string letterGrade = "";
+
+        if (numGrade >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (numGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (numGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (numGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"your grade is : {letterGrade}");
+
+        if (numGrade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else 
+        {
+            Console.WriteLine("Please focus more in study!");
+        }
+
+
+
     }
 }
