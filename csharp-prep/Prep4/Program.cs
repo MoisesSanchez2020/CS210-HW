2b80067e5d8a182587a6ea4519ceabb25a79cbea
MoisesSanchez2020 <san19039@byui.edu>
2023-04-18 23:07:06 -0700

    adding prep 4 first commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+      List<int> numbers = new List<int>();
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            int input;
+            do
+            {
+                Console.Write("Enter number: ");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input != 0)
+                {
+                    numbers.Add(input);
+                }
+            } while (input != 0);
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            double average = (double)sum / numbers.Count;
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine("The sum is: " + sum);
+            Console.WriteLine("The average is: " + average);
+            Console.WriteLine("The largest number is: " + max);
+        }
     }
-}
