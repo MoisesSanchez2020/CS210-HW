0f8c812892fe8394a7a4830c51bd4a107410a869
MoisesSanchez2020 <san19039@byui.edu>
2023-04-18 22:39:34 -0700

    commit prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string keepPlaying = "yes";
+        int guess = 0;
+
+        while (keepPlaying == "yes")
+        {
+            guess = 0;
+
+            Console.Write("What is the magic number? ");
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else 
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.Write("Do you wish to play again? ");
+            keepPlaying = Console.ReadLine();
+
+        }
     }
-}
+} 
