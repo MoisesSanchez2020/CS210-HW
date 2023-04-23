0fe8fbae649293da12b0bd6bfcf74af70972003d
MoisesSanchez2020 <san19039@byui.edu>
2023-04-22 23:08:30 -0700

    first entry Journal Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        UserInterface ui = new UserInterface(journal);
+
+        while (true)
+        {
+            ui.DisplayMenu();
+
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    ui.WriteNewEntry();
+                    break;
+                case "2":
+                    ui.DisplayJournal();
+                    break;
+                case "3":
+                    ui.SaveJournal();
+                    break;
+                case "4":
+                    ui.LoadJournal();
+                    break;
+                case "5":
+                   return;
+            default:
+            
+                Console.WriteLine("Invalid choice. Please enter a valid choice from the menu.");
+                break;
+        }
+    }
     }
 }
