12518db7cf47ed449bd3894a89d3ffe4427a15c3
MoisesSanchez2020 <san19039@byui.edu>
2023-05-16 18:20:34 -0700

    First update

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 5.0);
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
+        Circle circle = new Circle("Green", 3.0);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine("Color: " + shape.Color);
+            Console.WriteLine("Area: " + shape.GetArea());
+            Console.WriteLine();
+        }
     }
 }
