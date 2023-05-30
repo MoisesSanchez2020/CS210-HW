25953394886d47e7f2928a6cab928b0abd1393f6
MoisesSanchez2020 <san19039@byui.edu>
2023-05-29 22:30:32 -0700

    first program update final

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,176 @@
 using System;
 
-class Program
+// Base Event class
+public class Event
 {
-    static void Main(string[] args)
+    private string title;
+    private string description;
+    private DateTime date;
+    private TimeSpan time;
+    private Address address;
+
+    public Event(string title, string description, DateTime date, TimeSpan time, Address address)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.time = time;
+        this.address = address;
+    }
+
+    // Returns the standard details of the event
+    public string GetStandardDetails()
+    {
+        return $"Event Title: {title}\n" +
+               $"Description: {description}\n" +
+               $"Date: {date.ToShortDateString()}\n" +
+               $"Time: {time.ToString()}\n" +
+               $"Address: {address.GetAddressDetails()}";
+    }
+
+    // Returns the full details of the event (including specific details of derived event types)
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    // Returns a short description of the event
+    public virtual string GetShortDescription()
+    {
+        return $"Event Type: Generic Event\n" +
+               $"Event Title: {title}\n" +
+               $"Date: {date.ToShortDateString()}";
+    }
+}
+
+// Lecture class (derived from Event)
+public class Lecture : Event
+{
+    private string speaker;
+    private int capacity;
+
+    public Lecture(string title, string description, DateTime date, TimeSpan time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        this.speaker = speaker;
+        this.capacity = capacity;
+    }
+
+    // Overrides the GetFullDetails method to include lecture-specific details
+    public override string GetFullDetails()
+    {
+        return base.GetStandardDetails() +
+               $"Type: Lecture\n" +
+               $"Speaker: {speaker}\n" +
+               $"Capacity: {capacity}";
+    }
+}
+
+// Reception class (derived from Event)
+public class Reception : Event
+{
+    private string rsvpEmail;
+
+    public Reception(string title, string description, DateTime date, TimeSpan time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        this.rsvpEmail = rsvpEmail;
+    }
+
+    // Overrides the GetFullDetails method to include reception-specific details
+    public override string GetFullDetails()
+    {
+        return base.GetStandardDetails() +
+               $"Type: Reception\n" +
+               $"RSVP Email: {rsvpEmail}";
+    }
+}
+
+// OutdoorGathering class (derived from Event)
+public class OutdoorGathering : Event
+{
+    private string weatherForecast;
+
+    public OutdoorGathering(string title, string description, DateTime date, TimeSpan time, Address address, string weatherForecast)
+        : base(title, description, date, time, address)
+    {
+        this.weatherForecast = weatherForecast;
+    }
+
+    // Overrides the GetFullDetails method to include outdoor gathering-specific details
+    public override string GetFullDetails()
+    {
+        return base.GetStandardDetails() +
+               $"Type: Outdoor Gathering\n" +
+               $"Weather Forecast: {weatherForecast}";
+    }
+}
+
+// Address class
+public class Address
+{
+    private string street;
+    private string city;
+    private string state;
+    private string postalCode;
+
+    public Address(string street, string city, string state, string postalCode)
+    {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+    }
+
+    // Returns the address details as a formatted string
+    public string GetAddressDetails()
+    {
+        return $"Street: {street}\n" +
+               $"City: {city}\n" +
+               $"State: {state}\n" +
+               $"Postal Code: {postalCode}";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create instances of each event type
+        Address address1 = new Address("123 Main St", "Cityville", "Stateville", "12345");
+        Address address2 = new Address("456 Elm St", "Townville", "Stateville", "67890");
+
+        // Generic Event
+        Event genericEvent = new Event("Generic Event", "A generic event description.", DateTime.Now, TimeSpan.FromHours(2), address1);
+
+        // Lecture Event
+        Lecture lecture = new Lecture("Lecture Event", "A lecture event description.", DateTime.Now, TimeSpan.FromHours(3), address2, "John Doe", 100);
+
+        // Reception Event
+        Reception reception = new Reception("Reception Event", "A reception event description.", DateTime.Now, TimeSpan.FromHours(4), address1, "rsvp@example.com");
+
+        // Outdoor Gathering Event
+        OutdoorGathering outdoorGathering = new OutdoorGathering("Outdoor Gathering Event", "An outdoor gathering event description.", DateTime.Now, TimeSpan.FromHours(5), address2, "Sunny");
+
+        // Output the details of each event
+        Console.WriteLine("Generic Event:");
+        Console.WriteLine(genericEvent.GetStandardDetails());
+        Console.WriteLine(genericEvent.GetFullDetails());
+        Console.WriteLine(genericEvent.GetShortDescription());
+
+        Console.WriteLine("\nLecture Event:");
+        Console.WriteLine(lecture.GetStandardDetails());
+        Console.WriteLine(lecture.GetFullDetails());
+        Console.WriteLine(lecture.GetShortDescription());
+
+        Console.WriteLine("\nReception Event:");
+        Console.WriteLine(reception.GetStandardDetails());
+        Console.WriteLine(reception.GetFullDetails());
+        Console.WriteLine(reception.GetShortDescription());
+
+        Console.WriteLine("\nOutdoor Gathering Event:");
+        Console.WriteLine(outdoorGathering.GetStandardDetails());
+        Console.WriteLine(outdoorGathering.GetFullDetails());
+        Console.WriteLine(outdoorGathering.GetShortDescription());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,178 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public abstract class Activity
 {
-    static void Main(string[] args)
+    private DateTime date;      // Stores the date of the activity
+    private int duration;       // Stores the duration of the activity in minutes
+
+    public Activity(DateTime date, int duration)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        this.date = date;
+        this.duration = duration;
+    }
+
+    // Abstract methods to be implemented by derived classes
+    public abstract double GetDistance();     // Calculates and returns the distance of the activity
+    public abstract double GetSpeed();        // Calculates and returns the speed of the activity
+    public abstract double GetPace();         // Calculates and returns the pace of the activity
+
+    public string GetSummary()
+    {
+        double distance = GetDistance();
+        double speed = GetSpeed();
+        double pace = GetPace();
+
+        // Returns a summary string containing the activity details
+        return $"{date.ToString("dd MMM yyyy")} {GetType().Name} ({duration} min): " +
+               $"Distance: {distance.ToString("0.0")} {GetDistanceUnit()}, " +
+               $"Speed: {speed.ToString("0.0")} {GetSpeedUnit()}, " +
+               $"Pace: {pace.ToString("0.0")} {GetPaceUnit()}";
+    }
+
+    // Virtual methods with default implementations that can be overridden by derived classes
+    protected virtual string GetDistanceUnit()
+    {
+        return "units";
+    }
+
+    protected virtual string GetSpeedUnit()
+    {
+        return "units/h";
+    }
+
+    protected virtual string GetPaceUnit()
+    {
+        return "min/units";
+    }
+
+    // Calculate the total minutes from the duration
+    protected int GetMinutes()
+    {
+        return duration;
+    }
+}
+
+public class Running : Activity
+{
+    private double distance;      // Stores the distance covered during the run
+
+    public Running(DateTime date, int duration, double distance)
+        : base(date, duration)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return distance / (GetMinutes() / 60);
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / distance;
+    }
+}
+
+public class Cycling : Activity
+{
+    private double speed;      // Stores the speed of cycling in km/h
+
+    public Cycling(DateTime date, int duration, double speed)
+        : base(date, duration)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return speed * (GetMinutes() / 60);
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed;
+    }
+
+    protected override string GetSpeedUnit()
+    {
+        return "km/h";
+    }
+
+    protected override string GetPaceUnit()
+    {
+        return "min/km";
+    }
+}
+
+public class Swimming : Activity
+{
+    private int laps;      // Stores the number of laps swam
+
+    public Swimming(DateTime date, int duration, int laps)
+        : base(date, duration)
+    {
+        this.laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return laps * 50 / 1000;      // Converts laps to kilometers
+    }
+
+    public override double GetSpeed()
+    {
+        double distance = GetDistance();
+        return distance / (GetMinutes() / 60);
+    }
+
+    public override double GetPace()
+    {
+        double distance = GetDistance();
+        return GetMinutes() / distance;
+    }
+
+    protected override string GetDistanceUnit()
+    {
+        return "km";
+    }
+
+    protected override string GetPaceUnit()
+    {
+        return "min/km";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Activity> activities = new List<Activity>();
+
+        DateTime date = new DateTime(2022, 11, 3);
+        
+        // Creating instances of different activity types and adding them to the activities list
+        activities.Add(new Running(date, 30, 3.0));
+        activities.Add(new Running(date, 30, 4.8));
+        activities.Add(new Cycling(date, 30, 15.0));
+        activities.Add(new Cycling(date, 30, 24.1));
+        activities.Add(new Swimming(date, 30, 20));
+        activities.Add(new Swimming(date, 30, 40));
+
+        // Displaying the summary of each activity
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
